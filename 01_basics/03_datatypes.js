59cedb7013506f2deb159371c58cb8cea63a46b8
AJAY KANDPAL <ajay.kandpal1998@gmail.com>
2023-12-22 12:21:08 +0000

    Changes

---
+++ 01_basics/03_datatypes.js
@@ -0,0 +1,13 @@
+"use strict"; //to save old code from new JS features, and standard practise, treats as newer version of js
+//alert(3+3) //wont work as not using browser, using nodejs
+/* Primitive
+number (2^53), bigint, string, bool,
+null (standalone value, not 0, not undefined), undefined (declared but not assigned a value)
+symbol(used mostly in react to find uniqueness of components, also in figma) 
+ */
+
+//Object
+console.log(typeof 3); //number
+console.log(typeof "ajay"); //string
+console.log(typeof null); //output is object
+console.log(typeof undefined); //output is undefined
