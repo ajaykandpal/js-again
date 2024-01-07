3d26c18ff153c33b0735e1be8c7bd7c1a4f7906f
AJAY KANDPAL <ajay.kandpal1998@gmail.com>
2024-01-07 10:20:57 +0000

    control

---
+++ 03_basics/04_call_stack.js
@@ -0,0 +1,16 @@
+// JS Execution Context
+// watch this: https://youtu.be/ByhtOgF6uYM?si=nGpjUo1Rh_4_SsqS
+// 1: Global execution context (this keyword). diff for browser(window object) and node
+// all this happens in a thread.
+//JS is single threaded, verything is a process
+// 2: Function/Functional Execution Context
+// another execution context, in mongoose, etc. EVAL Execution Context. Basically a property of global EC
+
+// 2 phases to run JS program
+// 1. Memory Creation Phase (allocates memory to variables etc)
+// 2. Execution Phase (program executed)
+
+//Execution
+//Step1: Global execution/environment -> this keyword
+//Step2: Memory Phase
+//Step3: Execution Phase
